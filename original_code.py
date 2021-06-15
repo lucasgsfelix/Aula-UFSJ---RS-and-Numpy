22a48dc17e0e2e5628261e8427abe247fbb40326
lucasgsfelix <lucasgsfelix@gmail.com>
2021-06-15 11:06:16 -0300

    First commit

---
+++ original_code.py
@@ -0,0 +1,374 @@
+"""
+
+    Collaborative Filtering: Modeling Methods
+
+"""
+
+import random
+import math
+import time
+
+
+def select_similarity_metric(similarity_metric):
+    """
+        Selecting the similarity metric that is more suited to the problem
+
+        return a function of the choosen similarity metric
+    """
+
+    if similarity_metric == 'cosine':
+
+       return measure_cosine_similarity
+
+    assert "Unknow Similarity Measure"
+
+
+def measure_cosine_similarity(tokens_one, tokens_two):
+    """
+        Calculating the cosine similarity between two arrays
+
+        return a float value between -1 and 1
+    """
+
+    # measuring the numerator step
+    numerator = sum(list(map(lambda a, b: a * b, tokens_one, tokens_two)))
+
+    # denominator from the first token
+    denominator_one = math.sqrt(sum(list(map(lambda a: a**2, tokens_one))))
+
+    # denominator from the second token
+    denominator_two = math.sqrt(sum(list(map(lambda a: a**2, tokens_two))))
+
+    if denominator_one == 0 or denominator_two == 0:
+
+        return 0
+
+    return numerator/(denominator_one * denominator_two)
+
+
+def measure_similarity(tokens_one, tokens_two, similarity_metric='cosine'):
+    """
+        Receives as input two arrays with tokens (items or users) ratings
+
+        Example:
+
+            If the modeling is by user (The rating that a user has given to itens): 
+
+                User -> 3, 4, 1
+
+            If the modeling is by item (The Rating that a item has receive from differnent users):
+                
+                item -> 9, 4, 3, 4, 0
+
+        return a float value
+    """
+
+    similarity_method = select_similarity_metric(similarity_metric)
+
+    value = similarity_method(tokens_one, tokens_two)
+
+    return value
+
+
+def retrieve_neighbors(matrix, token_index, other_tokens, similarity_metric='cosine'):
+    """
+
+        Given a item or user, retrieve the closest neighbors
+
+    """
+
+    similarities = {}
+
+    for token, index in other_tokens.items():
+
+        similarities[token] = measure_similarity(matrix[token_index], matrix[index], similarity_metric)
+
+    # sorting a dictionary by values, we want reverse 
+    return {k: v for k, v in sorted(similarities.items(), key=lambda item: item[1], reverse=True)}
+
+
+def get_rating_based_on_closest_items(similarities, user_historic, user, mean, amount_neighbors=10):
+
+    similarity_sum, similarity_rating, index = 0, 0, 0
+
+    for item in similarities.keys(): # a dictionary user - items - rating
+
+        # what is the rating that my user has given to that similar item ?
+
+        if item not in user_historic[user].keys():
+
+            # if the item is not in the user historic, there is no way to measure it rating
+
+            continue
+
+        rating = user_historic[user][item]
+
+        similarity_rating += similarities[item] * rating 
+
+        similarity_sum += similarities[item]
+
+        if index == amount_neighbors:
+
+            break
+
+        index += 1
+
+
+    if similarity_sum == 0:
+
+        return mean
+
+    print(similarity_rating/similarity_sum)
+
+    return similarity_rating/similarity_sum
+
+
+def retrieve_unique_tokens(data):
+    """
+        Retrieve the unique tokens to model the matrix
+
+        return a dictionary of token and its unique items
+
+    """
+
+    tokens = {"users": list(set(list(map(lambda row: row[0], data)))),
+              "items": list(set(list(map(lambda row: row[1], data))))}
+
+    return tokens
+
+
+def define_prediction_features(prediction_data, modeling):
+
+    tokens = retrieve_unique_tokens(prediction_data)
+
+    # a dictionary of the modeling tokens a empty lists
+    tokens_info = dict(zip(tokens[modeling], [[]] * len(tokens[modeling])))
+
+    if modeling == 'items':
+
+        keys_index, values_index = 1, 0
+
+    else:
+
+        keys_index, values_index = 0, 1
+
+    for row in prediction_data:
+
+        tokens_info[row[keys_index]].append(row[values_index])
+
+    return tokens_info
+
+
+def measure_average_rating(data):
+
+
+    ratings_sum = 0
+
+    for row in data:
+
+        ratings_sum += float(row[2])
+
+    return ratings_sum/len(data)
+
+
+def generate_historic_data_matrix(historic_data, modeling, users, items, fill_zero=0):
+    """
+        Modeling the matrix of historical data
+
+        Modeling:
+
+            Define if the matrix generate will be a item x item or user x user modeling
+
+
+    """
+
+    # making a matrix of zeros
+    if modeling == 'items':
+
+        matrix = [[0] * len(users) for row in range(0, len(items))]
+
+    else:
+
+        matrix = [[0] * len(items) for row in range(0, len(users))]
+
+
+    for row in historic_data:
+
+        user = users[row[0]]
+        item = items[row[1]]
+
+        # rating given by the user
+        rating = int(row[2])
+
+        if rating == 0:
+
+            rating = fill_zero
+
+        if modeling == 'items':
+
+            matrix[item][user] = rating
+
+        else:
+
+            matrix[user][item] = rating
+
+    return matrix
+
+
+def retrieve_guide_features(historic_data):
+
+    tokens = retrieve_unique_tokens(historic_data)
+
+    users_items = {user: [] for user in tokens['users']}
+
+    # ratings, amount [0], [0]
+    items_ratings = dict(zip(tokens['items'], [[0, 0]] * len(tokens['items'])))
+    users_ratings = dict(zip(tokens['users'], [[0, 0]] * len(tokens['users'])))
+
+    for row in historic_data:
+
+        users_items[row[0]].append(row[1])
+
+        items_ratings[row[1]][0] += float(row[2])
+
+        users_ratings[row[0]][0] += float(row[2]) # summing the raitings
+
+        items_ratings[row[1]][1] += 1
+
+        users_ratings[row[0]][1] += 1
+
+
+    for user in users_ratings.keys():
+
+        users_ratings[user] = users_ratings[user][0]/users_ratings[user][1]
+
+    for item in items_ratings.keys():
+
+        items_ratings[item] = items_ratings[item][0]/items_ratings[item][1]
+
+
+    return users_items, define_index(tokens['users']), define_index(tokens['items']), users_ratings, items_ratings
+
+
+
+def define_user_item_rating(historic_data):
+
+    tokens = retrieve_unique_tokens(historic_data)
+
+    users_ratings = dict(zip(tokens['users'], [{} for _ in range(len(tokens['users']))]))
+
+    for row in historic_data:
+
+        users_ratings[row[0]][row[1]] = float(row[2])
+
+    return users_ratings
+
+
+def define_index(tokens):
+
+    return {token: index for index, token in enumerate(tokens)}
+
+
+def measure_ratings_by_nearest_neighbors(data, modeling='items'):
+
+    users_items, users, items, users_ratings, items_ratings = retrieve_guide_features(data['Historic Data'])
+
+    # a matrix users x items
+    historic_rating_matrix = generate_historic_data_matrix(data['Historic Data'], modeling, users, items)
+
+    # prediction data
+    modeling_tokens = define_prediction_features(data['Prediction Data'], modeling)
+
+    ratings_mean = measure_average_rating(data['Historic Data'])
+
+    users_ratings = define_user_item_rating(data['Historic Data'])
+
+    prediction_users = []
+
+    for user in modeling_tokens.values():
+
+        prediction_users.extend(user)
+
+    predictions = dict(zip(prediction_users, [{} for _ in prediction_users]))
+
+    for token, token_values in modeling_tokens.items():
+
+        if modeling == 'items':
+
+            if token in items:
+
+                similarities = retrieve_neighbors(historic_rating_matrix, items[token], items)
+
+            else:
+
+                for user in token_values:
+
+                    predictions[user][token] = ratings_mean
+
+            for user in token_values:
+
+                if user not in users.keys() or token not in items.keys():
+
+                    predicted_rating = ratings_mean
+
+                else:
+
+                    predicted_rating = get_rating_based_on_closest_items(similarities, users_ratings, user, ratings_mean)
+
+                predictions[user][token] = predicted_rating
+
+        elif modeling == 'users':
+
+            similarities = retrieve_neighbors(historic_rating_matrix, users[token], users)
+
+
+    return user_historic
+
+
+def read_table(file_input, sep=':', replace_char=None):
+    """
+        Read table with data
+
+        return a list of lists
+    """
+
+    with open(file_input, 'r') as read_input:
+
+        data = read_input.read()
+
+        if replace_char:
+
+            data = data.replace(replace_char, sep)
+
+        data = data.split('\n')
+
+        # removing the header
+        data.pop(0)
+
+    data = list(map(lambda row: row.split(sep), data))
+
+    return random.sample(list(filter(lambda row: row[0] != '', data)), 10000)
+    #return list(filter(lambda row: row[0] != '', data))
+
+
+if __name__ == '__main__':
+
+
+    input_arguments = {"Historic Data": read_table("Data/ratings.csv", ':', ','),
+                       "Prediction Data": read_table("Data/targets.csv", ':')}
+
+
+    output_file = "predictions.txt"
+
+    start = time.time()
+
+    with open("Data/time_reports.csv", "a+") as time_report:
+
+        measure_ratings_by_nearest_neighbors(input_arguments, modeling='items')
+
+        time_report.write('\t'.join([time.time() - start]) + '\n')
+
+
+    end = time.time()
+
+    print(end - start)
---
+++ updated_code.py
@@ -0,0 +1,266 @@
+"""
+
+    Collaborative Filtering: Modeling Methods
+
+"""
+
+import random
+import math
+import time
+
+import numpy as np
+import pandas as pd
+
+from operator import itemgetter
+
+
+def measure_cosine_similarity(tokens_one, tokens_two):
+    """
+        Calculating the cosine similarity between two arrays
+
+        return a float value between -1 and 1
+    """
+
+    return (np.dot(tokens_one, tokens_two)/(np.linalg.norm(tokens_one) * np.linalg.norm(tokens_two)))
+
+
+def get_rating_based_on_closest_items(similarity, adj_matrix, user, amount_neighbors=10):
+
+    similarity_sum, similarity_rating, index = 0, 0, 0
+
+    # the index order that sort that array
+    user_historic = np.where(adj_matrix[user] != 0)
+
+    # getting the biggest indexes where the rating is not 0
+    indexes = np.intersect1d(np.where(similarity != 0), user_historic)
+
+    sorted_indexes = [x for _, x in sorted(zip(similarity[indexes], indexes), key=lambda pair: pair[0], reverse=True)]
+
+    return (np.sum(adj_matrix[user][sorted_indexes] * similarity[sorted_indexes]))/np.sum(similarity[sorted_indexes])
+
+
+def retrieve_values_by_list(attributes_dict, list_keys):
+    """
+        Get the attributes of a dictionary given a list of keys
+
+    """
+
+    return list(itemgetter(*list_keys)(attributes_dict))
+
+
+def measure_items_similarity(data, adj_matrix, index_info):
+    """
+
+        Mount a similarity matrix between all tokens in the 
+
+    """
+
+    historic_items = data['Historic Data']['ItemId'].unique()
+    prediction_items = data['Prediction Data']['ItemId'].unique()
+
+    # retangular matrix
+    similarities_matrix = np.zeros((len(prediction_items), len(historic_items)))
+
+    print("Measure the distance matrix !")
+
+    #distance_matrix = np.matmul(items_matrix, items_matrix.T)
+
+    for item in prediction_items:
+
+        # users that want a rating in that specific item
+        users = data['Prediction Data'][data['Prediction Data']['ItemId'] == item]['UserId'].unique()
+
+        # getting the index of the users index
+        historic_index = data['Historic Data'][data['Historic Data']['UserId'].isin(users)]['ItemId'].unique()
+
+        #historic_index = unique_items
+
+        # return the index of the elements that are zero, i.e, where not filled yet
+        zero_indexes = np.argwhere(similarities_matrix[item] == 0).flatten()
+
+        # intersection == common values
+        # intersection between the indexes with zero
+        # and the historic indexes that we want to measure
+        historic_index = np.intersect1d(zero_indexes, historic_index)
+
+        # in this case all values are covered
+        if len(historic_index) == 0:
+
+            continue
+
+        # measuring the cosine distance between one item and all other
+        similarities = np.array(list(map(lambda index: measure_cosine_similarity(adj_matrix[index], adj_matrix[item]), historic_index)))
+
+        similarities_matrix[item, historic_index] = similarities
+
+        # now the indexes have inverted but our matrix is not quadratic
+        # we have to identify where is the data
+        
+        '''
+        historic_index = retrieve_values_by_list(index_info['test items'], historic_index)
+
+        if item in index_info['train item']:
+
+            item = index_info['train item'][item]
+
+            similarities_matrix[historic_index, item] = similarities
+
+        '''
+
+
+    return similarities_matrix
+
+
+def generate_historic_data_matrix(df, modeling, fill_zero=0):
+    """
+        Modeling the matrix of historical data
+
+        Modeling:
+
+            Define if the matrix generate will be a item x item or user x user modeling
+
+
+    """
+
+    places_info = {}
+
+    for token in set(df['ItemId']):
+
+
+        model_df = df[df['ItemId'] == token]
+
+        # Place Name: All Visitors of that profile and the rating
+        places_info[token] = dict(zip(model_df['UserId'], model_df['Prediction']))
+
+    df = pd.DataFrame(places_info).fillna(fill_zero)
+
+    if modeling == 'items':
+
+        return df.values
+
+    elif modeling == 'user':
+
+        return df.transpose().values
+
+    return df
+
+
+def measure_ratings_by_nearest_neighbors(data, index_info, modeling='items'):
+
+    # a matrix users x items
+    historic_rating_matrix = generate_historic_data_matrix(data['Historic Data'], modeling)
+
+    means = {}
+
+    # making a dict of token: rating - user mean and item mean
+    means['user'] = data['Historic Data'].groupby("UserId").mean('Prediction').to_dict()['ItemId']
+    means['item'] = data['Historic Data'].groupby("ItemId").mean('Prediction').to_dict()['UserId']
+
+    similarities = measure_items_similarity(data, historic_rating_matrix, index_info)
+
+    predictions = []
+
+    for item, user in data['Prediction Data'][['ItemId', 'UserId']].values:
+
+        similarity = similarities[item]
+
+        if user not in index_info['train users']: # cold start user
+
+            predicted_rating = means['item'][item]
+
+        elif item not in index_info['train items']: # cold start item
+
+            predicted_rating = means['user'][user]
+
+        else:
+
+            predicted_rating = get_rating_based_on_closest_items(similarity, historic_rating_matrix, user)
+
+            if np.isnan(predicted_rating):
+
+                predicted_rating = (means['item'][item] + means['user'][user])/2
+
+        predictions.append(predicted_rating)
+
+    return predictions
+
+
+def read_table(file_input, sep=':'):
+    """
+        Read table with data
+
+        return a list of lists
+    """
+
+    return pd.read_table(file_input, sep=sep, engine='python').sample(50000)
+
+
+
+def index_data(data):
+    """
+
+        Indexing all the data
+
+        Transforming all the values to number
+
+    """
+
+    index_info = {}
+
+    index_keys = {
+                    'train items': 'ItemId',
+                    'train users': 'UserId',
+                    'test items': 'ItemId',
+                    'test users': 'UserId'
+                 }
+
+
+    for key, value in index_keys.items():
+
+        data_token = 'Prediction'
+
+        if 'train' in key:
+
+            data_token = 'Historic'
+
+
+        # identifying the unique tokens
+        unique_values = data[data_token + " Data"][value].unique()
+
+        # making a index for each one of them
+        replace_dict = {value: index for index, value in enumerate(unique_values)}
+
+        # we will need this later
+        index_info[key] = {index: value for index, value in enumerate(unique_values)}
+
+        # replacing the value so it can be indexed
+        data[data_token + ' Data'][value] = data[data_token + ' Data'][value].apply(lambda token_id: replace_dict[token_id])
+
+
+    return data, index_info
+
+
+
+if __name__ == '__main__':
+
+
+    input_arguments = {"Historic Data": read_table("Data/ratings.csv", '[,:]'),
+                       "Prediction Data": read_table("Data/targets.csv", ':')}
+
+
+    input_arguments, index_info = index_data(input_arguments)
+
+    output_file = "predictions.txt"
+
+    start = time.time()
+
+    with open("Data/time_reports.csv", "a+") as time_report:
+
+        measure_ratings_by_nearest_neighbors(input_arguments, index_info, modeling='items')
+
+        time_report.write('\t'.join([str(time.time() - start)]) + '\n')
+
+
+    end = time.time()
+
+    print(end - start)
